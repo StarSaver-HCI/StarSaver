1c667be1e2b0dab20648ef26dcc0ce0926c61961
yonghanJu <ju990828@naver.com>
2022-05-17 02:07:51 +0900

    구글 이메일 연동 표시

--- app/src/main/java/com/hci/starsaver/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/hci/starsaver/ui/settings/SettingsFragment.kt
@@ -7,16 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.hci.starsaver.MainActivity
+import com.hci.starsaver.R
 import com.hci.starsaver.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingsBinding
     lateinit var auth: FirebaseAuth
+    lateinit var googleSignInClient:GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +44,17 @@ class SettingsFragment : Fragment() {
 
     private fun initAuth() {
         auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        val account = GoogleSignIn.getLastSignedInAccount(requireActivity())
+        if(account !== null){
+            binding.emailButton.text = account.email
+        }
     }
 
     private fun initLogOutButton() {
@@ -49,6 +65,9 @@ class SettingsFragment : Fragment() {
 
     private fun logOut() {
         auth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener(requireActivity()) {
+            //updateUI(null)
+        }
         val intent = Intent(this.context, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
