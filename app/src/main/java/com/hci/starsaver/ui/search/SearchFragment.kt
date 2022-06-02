d2eeb8f0f5ece35fae3e84ff97fcfbe0cfbb446e
yonghanJu <ju990828@naver.com>
2022-06-02 22:23:25 +0900

    Update SearchFragment.kt

--- app/src/main/java/com/hci/starsaver/ui/search/SearchFragment.kt
+++ app/src/main/java/com/hci/starsaver/ui/search/SearchFragment.kt
@@ -1,18 +1,24 @@
 package com.hci.starsaver.ui.search
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.hci.starsaver.R
 import com.hci.starsaver.data.bookMark.BookMark
 import com.hci.starsaver.databinding.FragmentSearchBinding
+import com.hci.starsaver.ui.editlink.EditLinkActivity
 import com.hci.starsaver.ui.home.HomeViewModel
 import com.hci.starsaver.util.LinkAdapter
 
@@ -65,6 +71,21 @@ class SearchFragment : Fragment() {
         binding.searchRecyclerView.adapter = adapter
         binding.searchRecyclerView.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
         binding.searchRecyclerView.visibility = View.GONE
+
+        adapter.setOnLinkClickedListener {
+            if (URLUtil.isValidUrl(it.link)) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(it.link)))
+            } else {
+                Toast.makeText(this.context, getString(R.string.checkUrl), Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        adapter.setOnDetailClickedListener {
+            val intent = Intent(this.context, EditLinkActivity::class.java)
+            intent.putExtra("BookMark", it.copy(bitmap = null))
+            startActivity(intent)
+        }
     }
 
     private fun roadRecyclerView() {
